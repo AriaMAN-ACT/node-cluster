3cc95a3370226762a56753330a76a3aa108d8113
WolfMAN <aria.azadi.pour@protonmail.com>
2020-05-05 16:22:46 +0430

    add pm2 to project

--- src/index.js
+++ src/index.js
@@ -1,21 +1,12 @@
-process.env.UV_THREADPOOL_SIZE = 1;
-const cluster = require('cluster');
-const os = require('os');
 const crypto = require('crypto');
+const express = require('express');
 
-if (cluster.isMaster) {
-    for (let i = 1; i < os.cpus().length; i++) {
-        cluster.fork();
-    }
-} else {
-    const express = require('express');
-    const app = express();
+const app = express();
 
-    app.get('/', (req, res) => {
-        crypto.pbkdf2('a', 'b', 100000, 512, 'sha512', (error, buffer) => {
-            res.send('buffer');
-        });
+app.get('/', (req, res) => {
+    crypto.pbkdf2('a', 'b', 100000, 512, 'sha512', (error, buffer) => {
+        res.send('buffer');
     });
+});
 
-    app.listen(3000);
-}
+app.listen(3000);
