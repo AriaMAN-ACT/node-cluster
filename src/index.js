32c9d7aba7052e553340728f90f9686d33adad8b
WolfMAN <aria.azadi.pour@protonmail.com>
2020-05-05 15:16:58 +0430

    use right amount of clusters

--- src/index.js
+++ src/index.js
@@ -1,13 +1,20 @@
+process.env.UV_THREADPOOL_SIZE = 1;
 const cluster = require('cluster');
+const os = require('os');
+const crypto = require('crypto');
 
 if (cluster.isMaster) {
-    cluster.fork();
+    for (let i = 1; i < os.cpus().length; i++) {
+        cluster.fork();
+    }
 } else {
     const express = require('express');
     const app = express();
 
     app.get('/', (req, res) => {
-        res.send('Hi There');
+        crypto.pbkdf2('a', 'b', 100000, 512, 'sha512', (error, buffer) => {
+            res.send('buffer');
+        });
     });
 
     app.listen(3000);
